0210e5de96357f09611a20cc15031a7f32c49911
dkbaffour777 <dkbaffour777@gmail.com>
2024-07-04 11:46:07 -0700

    Added header component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { TimesheetComponent } from '../timesheet/timesheet.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimesheetComponent],
+  imports: [HeaderComponent, TimesheetComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
