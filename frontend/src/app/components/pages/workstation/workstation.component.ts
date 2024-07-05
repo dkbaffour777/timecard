9f6a592b44ba21577c934730a63f0092acfd0c72
dkbaffour777 <dkbaffour777@gmail.com>
2024-07-05 13:03:48 -0700

    Update UI: workstation page

--- frontend/src/app/components/pages/workstation/workstation.component.ts
+++ frontend/src/app/components/pages/workstation/workstation.component.ts
@@ -1,6 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
+import {
+  MatBottomSheet,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -38,14 +46,6 @@ export class Member {
     this.break2in = 0;
     this.timeOut = 0;
   }
-
-  /* timeSpent(timestamp1: number, timestamp2: number): string {
-    if (!timestamp1 || !timestamp2) return '0';
-    const date1 = new Date(timestamp2);
-    const date2 = new Date(timestamp1);
-    const difference = getDateDifference(date1, date2);
-    return `${difference.hours} hours, ${difference.minutes} minutes, ${difference.seconds} seconds`;
-  } */
 }
 
 const fullnames = [
@@ -66,10 +66,46 @@ const fullnames = [
   'William Lewis',
 ];
 
+/**
+ * Bottom Sheet Component
+ */
+@Component({
+  selector: 'bottom-sheet',
+  templateUrl: './bottom-sheet.component.html',
+  styleUrl: './bottom-sheet.component.css',
+  standalone: true,
+  imports: [MatListModule, MatIconModule, MatButtonModule, MatTooltipModule, CommonModule, MatFormFieldModule, MatInputModule],
+})
+export class BottomSheetComponent {
+  members: Member[] = fullnames.map((fullname) => new Member(fullname));
+
+  constructor(
+    private _bottomSheetRef: MatBottomSheetRef<BottomSheetComponent>
+  ) {}
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+
+  trackByName(index: number, member: Member): string {
+    return member.name;
+  }
+}
+
+/**
+ * Workstation Component
+ */
 @Component({
   selector: 'app-workstation',
   standalone: true,
-  imports: [MatTableModule, MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
   templateUrl: './workstation.component.html',
   styleUrl: './workstation.component.css',
 })
@@ -77,18 +113,22 @@ export class WorkstationComponent implements AfterViewInit {
   dataSource: MatTableDataSource<Group>;
   displayedColumns: string[] = ['name', 'members'];
 
-  constructor() {
+  constructor(private _bottomSheet: MatBottomSheet) {
     const groups = [
       'Visual Inspection',
       'CalCheck',
       'Membrane',
       'Applicators',
       'Quality Assurance',
-      'Manufacturing Clerks'
+      'Manufacturing Clerks',
     ].map((name) => new Group(name));
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(groups);
   }
 
   ngAfterViewInit() {}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(BottomSheetComponent);
+  }
 }
